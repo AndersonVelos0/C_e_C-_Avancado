1bc5f67979a572e2dfa78e05438df0a208184b63
Anderson_Veloso <99103223+AndersonVelos0@users.noreply.github.com>
2023-04-01 11:30:58 -0300

    Update alocacaoDinamicaEmCpp.cpp
    Trocando as entradas e saidas pela sintaxe do C++

--- alocacao_de_memoria/alocacaoDinamicaEmCpp.cpp
+++ alocacao_de_memoria/alocacaoDinamicaEmCpp.cpp
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <new>
-
-
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
 int main(){
-
-    int tamanho, cont;
-
-    printf("Digite o tamanho do vetor: ");
-    scanf("%d", &tamanho);
-
-    int *vetor = new int(tamanho);
-
-    for (cont = 0; cont < tamanho; cont++){
-        vetor[cont] = cont;
-        printf("\n%d", vetor[cont]);
+ 
+    //Tamanho a ser criado
+    int tamanho,i;
+ 
+    //Lendo o tamanho do vetor
+    cout << "Digite o tamanho:";
+    cin >> tamanho;
+ 
+    //Mostrando o tamanho lido
+    cout << "Tamanho:" << tamanho;
+ 
+    //Criando um ponteiro que recebe o novo vetor vazio
+    int *vetor = new int[tamanho];
+ 
+    //Passa valores para o vetor e os imprime na tela
+    for(i = 0; i < tamanho; i++){
+        vetor[i] = i;
+        cout << "\n" << vetor[i];
     }
+ 
     return 0;
-
 }
